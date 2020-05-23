d0e479283e82fe251539a0eaa853b9e464249fb4
CodeDarigan <darigan@protonmail.com>
2020-05-23 19:12:28 +0100

    Add Equality Tests To Exercise Dynamic

--- addons/WAT/core/assertions/Asserts.cs
+++ addons/WAT/core/assertions/Asserts.cs
@@ -1,6 +1,11 @@
 using Godot;
 using System;
 
+namespace System.Runtime.CompilerServices
+{
+    public class ExtensionAttribute : Attribute { }
+}
+
 public class Asserts : Reference
 {
 	
@@ -23,4 +28,8 @@ public class Asserts : Reference
 		assertions.Call("is_false", a, context);
 	}
 	
+	public void IsEqual(dynamic a, dynamic b, String context = "")
+	{
+		assertions.Call("is_equal", a, b, context);
+	}
 }
