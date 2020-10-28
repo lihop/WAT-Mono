fc27c3d9b4104d9b2252c2fc9881cdfb86ce0321
AlexAbdDraw <darigan@protonmail.com>
2020-10-28 20:48:32 +0000

    Create Minimal Project

---
+++ SampleBuild.cs
@@ -0,0 +1,12 @@
+using Godot;
+using System;
+
+public class SampleBuild : Node
+{
+    // Make sure we can compile
+    public override void _Ready()
+    {
+        GD.Print("Hello World");
+        Console.WriteLine("Hello World");
+    }
+}
--- addons/WAT/namespace.gd
+++
@@ -1,5 +0,0 @@
-extends Reference
-class_name WAT
-
-const Test = preload("res://addons/WAT/core/test/test.gd")
-const TestSuiteOfSuites = preload("res://addons/WAT/core/test/suite.gd")
