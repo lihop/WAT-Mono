c7e5ba0392c87ff3ec89d3c393106d8fe06406a6
AlexAbdDraw <darigan@protonmail.com>
2020-10-30 21:24:16 +0000

    Add WAT Update

--- SampleBuild.cs
+++ SampleBuild.cs
@@ -3,10 +3,10 @@ using System;
 
 public class SampleBuild : Node
 {
-    // Make sure we can compile
-    public override void _Ready()
-    {
-        GD.Print("Hello World");
-        Console.WriteLine("Hello World");
-    }
+	// Make sure we can compile
+	public override void _Ready()
+	{
+		GD.Print("Hello World");
+		Console.WriteLine("Hello World");
+	}
 }
